3dcf1e1bdcd17df78e57db3eaeca3ce71838158d
dolphinderby <129580551+dolphinderby@users.noreply.github.com>
2023-04-22 10:08:42 +0000

    投稿から詳細画面への移動

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,22 @@
 class BooksController < ApplicationController
   def new
-    @list = Book.new
+    @book = Book.new
   end
+
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
