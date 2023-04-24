cc64431008b43244c078822b07ef947cdb3c0849
dolphinderby <129580551+dolphinderby@users.noreply.github.com>
2023-04-24 06:06:48 +0000

    レイアウト調整中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully created."
+      redirect_to '/books'
     else
+      @books = Book.all
       render :index
     end
   end
@@ -35,6 +37,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
